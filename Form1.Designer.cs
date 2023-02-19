e8cc09877a62905c6706a7a418fcb620b001d1c3
Vadik <vad09im@gmail.com>
2023-02-19 18:49:56 +0200

    v1.0

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -3,7 +3,7 @@
     partial class Form1
     {
         /// <summary>
-        /// Обязательная переменная конструктора.
+        /// Обязательная переменная конструктора. 
         /// </summary>
         private System.ComponentModel.IContainer components = null;
 
